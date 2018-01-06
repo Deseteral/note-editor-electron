f1073a4912c3752aabd2225d2b0c44f8d6e06e3d
Deseteral <deseteral@gmail.com>
2018-01-06 18:41:52 +0100

    File list prop types

--- src/components/FileList/FileList.jsx
+++ src/components/FileList/FileList.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 function FileList({ fileList }) {
   return (
     <ul>
-      {fileList.map(f => (<li style={({ color: 'green' })}>{f}</li>))}
+      {fileList.map(f => (<li>{f}</li>))}
     </ul>
   );
 }
+
+FileList.propTypes = {
+  fileList: PropTypes.arrayOf(PropTypes.string),
+};
+
+FileList.defaultProps = {
+  fileList: [],
+};
 
 export default FileList;
