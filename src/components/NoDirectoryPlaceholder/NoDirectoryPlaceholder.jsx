3fad97e131af18c621672567c6294c929c6714bb
Deseteral <deseteral@gmail.com>
2018-04-15 11:55:47 +0200

    Styling NoDirectoryPlaceholder

--- src/components/NoDirectoryPlaceholder/NoDirectoryPlaceholder.jsx
+++ src/components/NoDirectoryPlaceholder/NoDirectoryPlaceholder.jsx
@@ -1,11 +1,37 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
+import Button from 'material-ui/Button';
+import Icon from 'material-ui/Icon';
+import Typography from 'material-ui/Typography';
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const PlaceholderIcon = () => (
+  <Icon style={({ fontSize: '150px', color: 'var(--divider-color)' })}>
+    create_new_folder
+  </Icon>
+);
 
 function NoDirectoryPlaceholder({ onButtonClick }) {
   return (
-    <button onClick={() => onButtonClick()}>
-      Wybierz katalog
-    </button>
+    <Container>
+      <PlaceholderIcon />
+      <Typography color="textSecondary" paragraph>
+        Wygląda na to, że nie wybrano żadnego katalogu
+      </Typography>
+      <Button
+        color="primary"
+        variant="raised"
+        onClick={() => onButtonClick()}
+      >
+        Wybierz katalog
+      </Button>
+    </Container>
   );
 }
 
