a908de2d8ea80256987c1ba8cc646942cf5b7309
Deseteral <deseteral@gmail.com>
2018-08-29 16:49:17 +0200

    Removed unnecessary options from editor toolbar

--- src/components/TextEditor/TextEditor.jsx
+++ src/components/TextEditor/TextEditor.jsx
@@ -65,6 +65,25 @@ class TextEditor extends Component {
       border: 'none',
     };
 
+    const toolbarConfig = {
+      display: ['HISTORY_BUTTONS', 'INLINE_STYLE_BUTTONS', 'BLOCK_TYPE_DROPDOWN', 'BLOCK_TYPE_BUTTONS'],
+      INLINE_STYLE_BUTTONS: [
+        { label: 'Bold', style: 'BOLD', className: 'custom-css-class' },
+        { label: 'Italic', style: 'ITALIC' },
+        { label: 'Underline', style: 'UNDERLINE' },
+      ],
+      BLOCK_TYPE_DROPDOWN: [
+        { label: 'Normal', style: 'unstyled' },
+        { label: 'Heading Large', style: 'header-one' },
+        { label: 'Heading Medium', style: 'header-two' },
+        { label: 'Heading Small', style: 'header-three' },
+      ],
+      BLOCK_TYPE_BUTTONS: [
+        { label: 'UL', style: 'unordered-list-item' },
+        { label: 'OL', style: 'ordered-list-item' },
+      ],
+    };
+
     const onBackClick = editMode
       ? () => this.setState({ editMode: false })
       : () => changeView('FILE_LIST');
@@ -82,6 +101,7 @@ class TextEditor extends Component {
           )}
           <RichTextEditor
             rootStyle={rteStyle}
+            toolbarConfig={toolbarConfig}
             editorClassName="editor-root"
             value={value}
             readOnly={!editMode}
