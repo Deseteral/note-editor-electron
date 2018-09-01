89493d3bb36c997a73092621eb38acb5dce0749a
Deseteral <deseteral@gmail.com>
2018-09-01 14:46:41 +0200

    Simplified saga selector to be inline

--- src/saga/library-scanner.js
+++ src/saga/library-scanner.js
@@ -1,11 +1,10 @@
 import { call, put, select } from 'redux-saga/effects';
-import { getLibraryPath } from './selectors';
 import libraryScanner from '../services/library-scanner';
 import { libraryScannerFinished } from '../actions/library-scanner';
 
 function* libraryScannerStart() {
   try {
-    const libraryPath = yield select(getLibraryPath);
+    const libraryPath = yield select(state => state.libraryPath);
     const files = yield call(libraryScanner, libraryPath);
     yield put(libraryScannerFinished(files));
   } catch (e) {
