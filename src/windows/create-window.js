ca48188fb4b8181aef04176e749676ac5d929770
Deseteral <deseteral@gmail.com>
2018-09-03 19:54:16 +0200

    Removed menu bar I guess?

--- src/windows/create-window.js
+++ src/windows/create-window.js
@@ -5,6 +5,8 @@ function createWindow(name, options) {
   windows[name] = new BrowserWindow(options);
   windows[name].loadURL(`file://${__dirname}/${name}/index.html`);
 
+  windows[name].setMenu(null);
+
   windows[name].on('closed', () => {
     windows[name] = null;
   });
